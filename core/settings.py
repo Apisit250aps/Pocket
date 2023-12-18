16449826b32a8fff62675609d7b9170837cc2cd7
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2023-12-18 16:31:58 +0700

    update

--- core/settings.py
+++ core/settings.py
@@ -86,12 +86,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "verceldb",
+        "USER": "default",
+        "PASSWORD": "7vcOVT9sfqQD",
+        "HOST": "ep-bitter-lab-15701778-pooler.us-east-1.postgres.vercel-storage.com",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
